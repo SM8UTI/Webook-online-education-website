b4238fd8febd9ab263d5bf2ad24a1a55dd4a6267
Smruti Ranjan Nayak <56423859+SM8UTI@users.noreply.github.com>
2022-07-02 21:23:05 +0530

    navbar add ( not responsive )

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,10 @@
-console.log("hello world!")
+
+
+// nav bar 
+
+const primaryNav = document.querySelector('.primary-navbar');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click',()=>{
+
+});
