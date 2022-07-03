edfe9d91a204eb4f11bdb69fd1b402b711f2390b
Smruti Ranjan Nayak <56423859+SM8UTI@users.noreply.github.com>
2022-07-03 10:56:53 +0530

    Responsive navbar update code

---
+++ README.md
@@ -0,0 +1,5 @@
+## Webook | Online Single page Education Website 
+
+</br>
+
+### Demo :  [Link](https://sm8uti.github.io/Webook-online-education-website/)
--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,19 @@
-
-
 // nav bar 
 
-const primaryNav = document.querySelector('.primary-navbar');
-const navToggle = document.querySelector('.mobile-nav-toggle');
 
-navToggle.addEventListener('click',()=>{
 
-});
+const header = document.getElementById('header');
+const toggle = document.getElementById('toggle');
+const navbar = document.getElementById('primary-navbar');
+
+document.onclick = function(e){
+    if(e.target.id !== 'header' && e.target.id !== 'toggle' && e.target.id !== 'primary-navbar' ){
+        toggle.classList.remove('active');
+        navbar.classList.remove('active');
+    };
+};
+
+toggle.addEventListener('click',()=>{
+    toggle.classList.toggle('active');
+    navbar.classList.toggle('active');
+})
